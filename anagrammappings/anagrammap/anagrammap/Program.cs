cac07d0f57adeebbec9331a129888b6842c7f09f
Paul Deines <paul.deines@gmail.com>
2019-01-06 19:53:35 -0800

    Anagram Mappings - Initial Solution
    Brute force method.
    
    problem from : https://leetcode.com/problems/find-anagram-mappings/

---
+++ anagrammappings/anagrammap/anagrammap/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anagrammap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] {12,28,46,32,50};
+            int[] b = new int[] {50,12,32,46,28};
+
+            int[] p = AnagramMappings(a, b);
+            foreach (int x in p)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        static public int[] AnagramMappings(int[] A, int[] B)
+        {
+            int[] p = new int[A.Length];
+            for(int i = 0; i < A.Length; i++)
+            {
+                for (int j = 0; j < B.Length; j++)
+                {
+                    if (B[j] == A[i])
+                    {
+                        p[i] = j;
+                        continue;
+                    }
+                }
+            }
+
+            return p;
+        }
+    }
+}
