559dadd73536936c0df92eec07ed5dca0f349fc6
Paul Deines <paul.deines@gmail.com>
2019-01-11 20:13:50 -0800

    NestedList Weight Sum
    https://leetcode.com/problems/nested-list-weight-sum/

---
+++ nestedlist/nestetlist/nestetlist/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nestetlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int DepthSum(IList<NestedInteger> nestedList)
+        {
+            return ComputeWeight(nestedList, 1);
+        }
+    
+        public int ComputeWeight(IList<NestedInteger> nestedList, int depth)
+        {
+            int sum = 0;
+            foreach(NestedInteger ni in nestedList)
+            {
+                if(ni.IsInteger())
+                {
+                    sum += depth * ni.GetInteger();
+                }
+                else
+                {
+                    sum += ComputeWeight(ni.GetList(), depth + 1);
+                }
+            }
+            return sum;
+        }
+    }
+}
