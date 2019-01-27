8d566c7683b065fadaa3e3b4946bb6eea4086cb5
Paul Deines <paul.deines@gmail.com>
2019-01-26 20:53:58 -0800

    N-Repeated Element in Size 2N Array
    https://leetcode.com/problems/n-repeated-element-in-size-2n-array/

---
+++ repeatedelement/repeatelement/repeatelement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repeatelement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input1 = new int[] {1,2,3,3};
+            int[] input2 = new int[] {2,1,2,5,3,2};
+
+            int output = RepeatedNTimes(input2);
+            Console.WriteLine(output);
+        }
+
+        public static int RepeatedNTimes(int[] A)
+        {
+            Dictionary<int,int> map = new Dictionary<int, int>();
+
+            foreach (int i in A)
+            {
+                if (map.ContainsKey(i))
+                {
+                    return i;
+                }
+                else
+                {
+                    map.Add(i,i);
+                }
+            }
+
+            return -1;
+        }
+    }
+}
