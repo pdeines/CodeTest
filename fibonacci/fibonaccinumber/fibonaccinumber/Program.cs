90ee4b2ce4bcc9c332419091e24e4206cc6de946
Paul Deines <paul.deines@gmail.com>
2019-02-16 06:42:56 -0800

    FibonacciNumber - brute force solution
    https://leetcode.com/problems/fibonacci-number/

---
+++ fibonacci/fibonaccinumber/fibonaccinumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonaccinumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fib(2));
+            Console.WriteLine(fib(3));
+            Console.WriteLine(fib(4));
+        }
+
+        static int fib(int N)
+        {
+            if (N == 0)
+            {
+                return 0;
+            }
+
+            if (N == 1)
+            {
+                return 1;
+            }
+
+            return fib(N - 2) + fib(N - 1);
+        }
+
+
+    }
+}
