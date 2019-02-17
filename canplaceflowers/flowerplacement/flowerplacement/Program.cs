0a3c5ff6ab9b62ac3edb80d967060784b0a53eae
Paul Deines <paul.deines@gmail.com>
2019-02-17 13:24:09 -0800

    CanPlaceFlowers
    https://leetcode.com/problems/can-place-flowers/

---
+++ canplaceflowers/flowerplacement/flowerplacement/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace flowerplacement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input1 = new[] {1, 0, 0, 0, 1};
+            int[] input2 = new[] {1, 0, 0, 0, 0, 1};
+            int[] input3 = new[] {1, 0, 1, 0, 1, 0, 1};
+            int[] input4 = new[] {1, 0, 0, 0, 0};
+            int n = 2;
+
+            bool output = CanPlaceFlowers(input4, n);
+
+            Console.WriteLine(output);
+        }
+
+        public static bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            int flowersPlaced = 0;
+            int index = 0;
+
+            while (index < flowerbed.Length)
+            {
+                if (flowerbed[index] == 0 && (index == 0 || flowerbed[index - 1] == 0) &&
+                    (index == flowerbed.Length - 1 || flowerbed[index + 1] == 0))
+                {
+                    flowerbed[index] = 1;
+                    flowersPlaced++;
+                }
+
+                index++;
+            }
+
+            if (flowersPlaced >= n)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
