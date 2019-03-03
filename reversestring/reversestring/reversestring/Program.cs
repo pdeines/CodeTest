66fc259ba3496dac4a051ce5eac9d85ea39e280e
Paul Deines <paul.deines@gmail.com>
2019-03-03 11:34:14 -0800

    ReverseString
    https://leetcode.com/problems/reverse-string/

---
+++ reversestring/reversestring/reversestring/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace reversestring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input1 = new[] {'h', 'e', 'l', 'l', 'o'};
+            char[] input2 = new[] {'H', 'a', 'n', 'n', 'a', 'h'};
+
+            ReverseString(input2);
+            Console.WriteLine(input2);
+        }
+
+        public static void ReverseString(char[] s) 
+        {
+            if (s.Length <= 1)
+            {
+                return;
+            }
+
+            int frontIndex = 0;
+            int backIndex = s.Length - 1;
+
+            while (frontIndex < backIndex)
+            {
+                char temp = s[frontIndex];
+                s[frontIndex] = s[backIndex];
+                s[backIndex] = temp;
+                frontIndex++;
+                backIndex--;
+            }
+        }
+    }
+}
